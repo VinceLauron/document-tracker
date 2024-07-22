b50492efd708cdf9c733734acb23e098c7649c34
Vince Lauron <vince@example.com>
2024-07-22 10:43:04 +0800

    new

--- applicant/db_connect.php
+++ applicant/db_connect.php
@@ -1,23 +1,3 @@
 <?php 
 
-// Enable error reporting for debugging purposes
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-// Define database credentials
-define('DB_SERVER', "localhost");
-define('DB_USERNAME', "u510162695_fms_db_root");
-define('DB_PASSWORD', "1Fms_db_root");
-define('DB_DATABASE', "u510162695_fms_db");
-
-// Attempt to connect to MySQL database
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connection successful!";
-}
-
-?>
+$conn= new mysqli('localhost','u510162695_fms_db_root','1Fms_db_root','u510162695_fms_db')or die("Could not connect to mysql".mysqli_error($con));
