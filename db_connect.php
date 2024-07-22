b7ce933f139bfc1e8f63420080d61e913dcbcf49
Vince Lauron <vince@example.com>
2024-07-22 10:19:01 +0800

    commit yes

--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,14 @@
 <?php 
 
-$conn= new mysqli('127.0.0.1/3306','u510162695_fms_db_root','1Fms_db_root','u510162695_fms_db')or die("Could not connect to mysql".mysqli_error($con));
+define('DB_SERVER',"localhost");
+define('DB_USERNAME',"u510162695_fms_db_root");
+define('DB_PASSWORD',"1Fms_db_root");
+define('DB_DATABASE',"u510162695_fms_db");
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if(!$conn){
+    die("Connection Failed: ". mysqli_connect_error());
+}
+
+?>
