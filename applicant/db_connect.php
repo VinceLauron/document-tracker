26dcaa0b3bebc45122296127a1db9a1201b08cca
Vince Lauron <vince@example.com>
2024-07-22 10:53:08 +0800

    new data

--- applicant/db_connect.php
+++ applicant/db_connect.php
@@ -1,3 +1,12 @@
-<?php 
+<?php
+$servername = "localhost";
+$username = "u510162695_fms_db_root"; 
+$password = "1Fms_db_root";
+$dbname = "u510162695_fms_db"; 
 
-$conn= new mysqli('localhost','u510162695_fms_db_root','1Fms_db_root','u510162695_fms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
